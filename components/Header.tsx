113c815ae044aefb9b3a4f908a953cd026a21076
smartvision11th-spec <smartvision11th@gmail.com>
2026-09-17 19:04:03 +0530

    Refactor Header component with improved links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,1 +1,19 @@
-import Link from "next/link";export function Header(){return <header><Link className="logo" href="/">ART<span>SELL</span></Link><nav><Link href="/shop">Shop</Link><Link href="/about">About</Link><Link href="/admin">Admin</Link><Link href="/cart">Bag</Link></nav></header>}
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header>
+      <Link className="logo" href="/">
+        ARTIST <span>DIPALI SINGH</span>
+      </Link>
+
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/shop">Gallery</Link>
+        <Link href="/about">About</Link>
+        <Link href="/commission">Commissions</Link>
+        <Link href="/cart">Bag</Link>
+      </nav>
+    </header>
+  );
+}
